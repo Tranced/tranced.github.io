ac8c37854296d59b2bebf4bba3b7428710b13c59
Tranced <terhyang@gmail.com>
2019-09-29 15:07:55 -0700

    lint the js

--- much-fast.js
+++ much-fast.js
@@ -1,12 +1,11 @@
-let goFaster = document.getElementsByClassName('marquee');
-let greetings = ["sup", "kek", "lel", "howdy", "swag"];
-let doge = document.getElementsByClassName('doge-face');
-for (let element of goFaster){
-    element.addEventListener('mouseover', function(e){
-        let randSeed = Math.random();
-        element.style.animation = `marquee ${randSeed*8}s linear infinite`;
-        let textToChange = element.childNodes[0];
-        let newText = document.createTextNode(greetings[Math.floor(randSeed*greetings.length)]);
-        element.replaceChild(newText, textToChange);
-    })
+const goFaster = document.getElementsByClassName('marquee')
+const greetings = ['sup', 'kek', 'lel', 'howdy', 'swag']
+for (const element of goFaster) {
+  element.addEventListener('mouseover', function (e) {
+    const randSeed = Math.random()
+    element.style.animation = `marquee ${randSeed * 8}s linear infinite`
+    const textToChange = element.childNodes[0]
+    const newText = document.createTextNode(greetings[Math.floor(randSeed * greetings.length)])
+    element.replaceChild(newText, textToChange)
+  })
 }
